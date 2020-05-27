b1e709dce7c9be091ddcd41b3b189159ff8b99c5
k1ebe <audunhellner@gmail.com>
2020-05-27 20:07:42 +0200

    More twiddling with keymap.

--- keyboards/planck/keymaps/k1ebe/keymap.c
+++ keyboards/planck/keymaps/k1ebe/keymap.c
@@ -35,6 +35,19 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
+enum combos {
+  TABQ_1,
+  PBKS_aa
+  };
+
+const uint16_t PROGMEM tabq_combo[] = {KC_TAB, KC_Q, COMBO_END};
+const uint16_t PROGMEM pbks_combo[] = {KC_P, KC_BSPC, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [TABQ_1] = COMBO(tabq_combo, KC_1),
+  [PBKS_aa] = COMBO(pbks_combo, KC_LBRC)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -101,14 +114,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | End  |Pg Dn |Pg Up | Home |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDN, KC_PGUP, KC_HOME
 ),
 
 
--- keyboards/planck/keymaps/k1ebe/readme.md
+++ keyboards/planck/keymaps/k1ebe/readme.md
@@ -8,11 +8,19 @@ This layer is accessed while holding down SPACE and gives easy access to F-keys 
 
 ## To do
 
+Finding a solution to å is a top priority. Raise + l is pt å and Lower + l is Å.
+
 ### Combo keys
+
+TAB+Q is 1
+P+Backspace is å
 
 https://docs.qmk.fm/#/feature_combo
 
-Use combo keys for CTRL+V and CTRL+C
-æøå kan nok også settes som komboer
-Can the top row be set to combos for numbers? Ie QW becomes 1 and so forth. Or maybe QA is better...
+- Use combo keys for CTRL+V and CTRL+C
+- æøå kan nok også settes som komboer
+- Can the top row be set to combos for numbers? Ie QW becomes 1 and so forth. Or maybe QA is better...
 
+## Random thoughts
+
+Swap OS key with the blank left on Control perhaps.
